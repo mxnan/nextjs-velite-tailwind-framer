bfae5819a4cda06a635742f2da6bb059af82ed89
mxnan <128995981+mxnan@users.noreply.github.com>
2024-10-13 20:19:28 +0530

    :hammer:  updated videoWithSrc to handle watermark cover
    - using slant-it folder  for video showcasing

--- app/(pages)/demo/page.tsx
+++ app/(pages)/demo/page.tsx
@@ -1,15 +1,14 @@
-
-import ImageCard from '@/showcase/demo/playground/image-card'
-import React from 'react'
+import ImageCard from "@/showcase/demo/playground/image-card";
+import React from "react";
 
 export default function DemoPage() {
   return (
-    <div className='min-h-screen pt-10 max-w-7xl mx-auto w-full flex flex-col items-center justify-center'>
-      <div className='text-center pb-4 border-b border-gray-700'>
-        <h1 className='text-4xl font-semibold'>Demo Page</h1>
-        <p className='text-lg mt-4'>This is just a demo page for videography</p>
+    <div className="pt-10 max-w-7xl mx-auto w-full flex flex-col items-center justify-center">
+      <div className="text-center pb-4 border-b border-gray-700">
+        <h1 className="text-4xl font-semibold">Demo Page</h1>
+        <p className="text-lg mt-4">This is just a demo page for videography</p>
       </div>
       <ImageCard />
     </div>
-  )
+  );
 }
--- components/mdx/video-from-src.tsx
+++ components/mdx/video-from-src.tsx
@@ -1,4 +1,7 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import { Icons } from "../icons";
 
 export default function VideoFromSrc({
   src,
@@ -6,14 +9,27 @@ export default function VideoFromSrc({
   className,
 }: {
   src: string;
-  title?: string;
+  title: string;
   className?: string;
 }) {
+  const initial = {
+    width: "200px",
+  };
+
+  const animate = {
+    width: "auto",
+    transition: {
+      delay: 0.3,
+      duration: 1,
+      type: "tween",
+      stiffness: 100,
+      damping: 30,
+    },
+  };
   return (
     <div className={className}>
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        
         autoPlay
         muted
         loop
@@ -22,6 +38,16 @@ export default function VideoFromSrc({
         <source src={src} />
         Your browser does not support the video tag.
       </video>
+      <motion.span
+        whileInView={animate}
+        initial={initial}
+        className="absolute z-20 -bottom-2 -right-2 
+         min-w-max rounded-2xl p-3 pb-5 pr-5 text-sm font-medium bg-secondary
+          text-indigo-600 dark:text-lime-400 flex items-center gap-2
+          "
+      >
+        <Icons.chevronDown />  Image Card
+      </motion.span>
     </div>
   );
 }
