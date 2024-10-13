d2a104426c8c506ef54bdb66dcd8dac793c664ee
mxnan <128995981+mxnan@users.noreply.github.com>
2024-10-13 17:16:23 +0530

    :hammer:  sidebar updates
    - using slantit videos in marquee .
    - using demo page for videography

--- app/(pages)/demo/page.tsx
+++ app/(pages)/demo/page.tsx
@@ -1,10 +1,15 @@
-import AccordionDemo from '@/showcase/demo/blocks/accordion-demo'
+
+import ImageCard from '@/showcase/demo/playground/image-card'
 import React from 'react'
 
 export default function DemoPage() {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center'>
-       <AccordionDemo />
+    <div className='min-h-screen pt-10 max-w-7xl mx-auto w-full flex flex-col items-center justify-center'>
+      <div className='text-center pb-4 border-b border-gray-700'>
+        <h1 className='text-4xl font-semibold'>Demo Page</h1>
+        <p className='text-lg mt-4'>This is just a demo page for videography</p>
+      </div>
+      <ImageCard />
     </div>
   )
 }
